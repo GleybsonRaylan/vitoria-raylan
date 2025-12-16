be53d7e64ed18d84b88963593749e115bfdda6b4
GleybsonRaylan <gleybsonraylan39@gmail.com>
2025-12-16 19:30:14 -0300

    TE AMO

--- src/components/FeedbackModal.tsx
+++ src/components/FeedbackModal.tsx
@@ -10,8 +10,8 @@ interface FeedbackModalProps {
 }
 
 // Placeholder images - you can replace these URLs with your own photos
-const CORRECT_IMAGE = "/public/Certa.jpg";
-const WRONG_IMAGE = "/public/Errada.jpg";
+const CORRECT_IMAGE = "/public/certa.jpg";
+const WRONG_IMAGE = "/public/errada.jpg";
 
 export const FeedbackModal = ({
   isCorrect,
